b55b48e7e7231a00d94fe95721e37770dcbdd0d5
Thalia Danielle <31187727+thaliadani@users.noreply.github.com>
2026-02-26 02:12:07 -0300

    alterado

--- src/assets/scripts/menu-hamburguer.js
+++ src/assets/scripts/menu-hamburguer.js
@@ -1,11 +1,20 @@
-const btnMenuHamburguer = document.getElementById('btn-menu-hamburguer');
-const menu = document.getElementById('menu');
-
-
 function abrirMenu() {
-    if (menu.style.display === 'flex') {
-        menu.style.display = 'none';
+    const menu = document.getElementById('menu');
+    const body = document.body;
+
+    menu.classList.toggle('ativo');
+
+    if (menu.classList.contains('ativo')) {
+        body.style.overflow = 'hidden';
     } else {
-        menu.style.display = 'flex';
+        body.style.overflow = 'auto';
     }
 }
+
+document.querySelectorAll('#menu li a').forEach(link => {
+    link.addEventListener('click', () => {
+        const menu = document.getElementById('menu');
+        menu.classList.remove('ativo');
+        document.body.style.overflow = 'auto';
+    });
+});
